0172622e2b77b10912cc7473de9bb585f5ef3b2e
Craig Watson <craig@watsons.ch>
2020-05-06 21:26:36 -0400

    Unit tests for new communication protocol
    routine controller tests currently disabled since they are out
    of date

---
+++ test/testcommunicator.h
@@ -0,0 +1,37 @@
+#include <QtTest/QtTest>
+#include <QtCore/QDebug>
+
+#include "serialcommunicator.h"
+
+class TestCommunicator : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+
+    void init();
+    void cleanup();
+
+    void decodeSimpleValidSequence();
+    void decodeValidSequenceWithLeadingJunk();
+    void decodeValidSequenceWithEscapes();
+    void decodeJunk();
+    void decodeFragmentedMessage();
+    void decodeSeveralMessages();
+
+    void valveChange();
+    void pumpChange();
+    void pressureChange();
+
+    void frameMessage();
+
+    // To do:
+    // void uptime();
+    // void error();
+
+private:
+
+    SerialCommunicator * c;
+};
